a8ff336aad6c0b927a71b76540bcead5f82bb334
jwbackhouse <jwbackhouse@gmail.com>
2020-10-02 18:48:05 +0000

    Refactor log controller code.

--- src/controllers/log.js
+++ src/controllers/log.js
@@ -7,31 +7,21 @@ exports.get = (req, res) => {
 exports.post = async(req, res) => {
   const existingLog = req.user.log;
   const data = req.body;
-  let keys = Object.keys(req.body);
-  let IDs = keys.slice(1);
-  let drinks = [];
 
   try {
     if (data.date === '') throw new Error('Please choose a date');
 
-    for (let i = 0; i < IDs.length; i++) {
-      const drinkId = IDs[i];
-      if (data[drinkId] > 0) {
-        drinks.push({
-          drinkId,
-          quantity: data[drinkId],
-        });
-      }
-    }
+    let keys = Object.keys(req.body);
+    let IDs = keys.slice(1);
+    const drinks = createDrinkArr(IDs, data);
     const log = { date: data.date, drinks };
 
     const idx = existingLog.findIndex(entry => {
       return entry.date.toISOString().slice(0, 10) === log.date;
     });
+
     if (idx !== -1) {
-      // TODO: split out into separate function
-      const matchingLog = existingLog[idx];
-      const existingDrinks = matchingLog.drinks;
+      const existingDrinks = existingLog[idx].drinks;
 
       for (let i = 0; i < drinks.length; i++) {
         const drinkIdx = existingDrinks.findIndex(drink => drink.drinkId.toString() === drinks[i].drinkId.toString());
@@ -48,6 +38,22 @@ exports.post = async(req, res) => {
     res.send();
   } catch (err) {
     console.log(err.message);
-    res.render('drink_form', { error: err.message });
+    res.render('log', { data, error: err.message });
   }
+};
+
+const createDrinkArr = (IDs, data) => {
+  let drinks = [];
+
+  for (let i = 0; i < IDs.length; i++) {
+    const drinkId = IDs[i];
+    if (data[drinkId] > 0) {
+      drinks.push({
+        drinkId,
+        quantity: data[drinkId],
+      });
+    }
+  }
+
+  return drinks;
 };
