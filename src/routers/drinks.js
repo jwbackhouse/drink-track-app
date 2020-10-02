ea0dd39596a073c32b044b807f5f7cee0c2f435b
jwbackhouse <jwbackhouse@gmail.com>
2020-10-02 10:16:09 +0000

    Tidy up code.

--- src/routers/drinks.js
+++ src/routers/drinks.js
@@ -5,15 +5,11 @@ const auth = require('../middleware/auth.js');
 const router = new express.Router();
 
 router.get('/drinks', auth, drink_controller.all_get);
-
 router.get('/add-drink', auth, drink_controller.create_get);
 router.post('/add-drink', auth, drink_controller.create_post);
-
 router.delete('/drinks/all', drink_controller.all_delete); // FOR DEV PURPOSES ONLY
-
-router.get('/drinks/:id', auth, drink_controller.get);
-router.put('/drinks/:id', auth, drink_controller.put);
-router.delete('/drinks/:id', auth, drink_controller.delete);
-
+router.get('/drinks/:id', auth, drink_controller.drink_get);
+router.put('/drinks/:id', auth, drink_controller.drink_put);
+router.delete('/drinks/:id', auth, drink_controller.drink_delete);
 
 module.exports = router;
