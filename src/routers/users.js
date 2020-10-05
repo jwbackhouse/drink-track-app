60fa27ec9fae0e9a1ef9f115675c27bca9589fb9
jwbackhouse <jwbackhouse@gmail.com>
2020-10-05 12:11:54 +0000

    Tidy up code.

--- src/routers/users.js
+++ src/routers/users.js
@@ -6,14 +6,11 @@ const user_controller = require('../controllers/users.js');
 const router = new express.Router();
 const upload = multer({
   // dest: 'avatars/',  // if no dest specified, buffer is passed to next function
-  limits: {
-    fileSize: 1000000,  // number of bytes
-  },
+  limits: { fileSize: 1000000 }, // number of bytes
   fileFilter(req, file, cb) {
     if (!file.originalname.match(/\.(jpe?g|png)$/)) {
       return cb(new Error('Please choose a jpg or png file.'));
     }
-
     cb(null, true);
   },
 });
@@ -22,28 +19,18 @@ router.post('/users', user_controller.index);
 
 router.get('/users/login', user_controller.login_get);
 router.post('/users/login', user_controller.login_post);
-
 router.post('/users/logout', auth, user_controller.logout);
-
 router.post('/users/logout-all', auth, user_controller.logout_all);
 
-router.post('/users/me/avatar', auth, upload.single('avatar'), user_controller.avatar_post);
-
 router.get('/users/me', auth, user_controller.get);
-
-router.get('/users/:id/avatar', user_controller.avatar_get);
-
 router.patch('/users/me', auth, user_controller.patch);
-
 router.delete('/users/me', auth, user_controller.delete);
-
+router.post('/users/me/avatar', auth, upload.single('avatar'), user_controller.avatar_post);
 router.delete('/users/me/avatar', auth, user_controller.avatar_delete);
-
+router.get('/users/:id/avatar', user_controller.avatar_get);
 
 // FOR DEV PURPOSES ONLY
 router.get('/users', user_controller.all_get);
-
 router.delete('/users', user_controller.all_delete);
-
 
 module.exports = router;
